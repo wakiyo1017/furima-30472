7832a8ea909adf50a22c44d657c090371c8e4c3c
wakiyo1017 <71360557+wakiyo1017@users.noreply.github.com>
2020-10-06 12:38:09 +0900

    deviseの導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-root to: 'items#index'
+  devise_for :users
+  root to: 'items#index'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
