961b4b84c5a8eae184a100dd9d9513c27683e217
wakiyo1017 <71360557+wakiyo1017@users.noreply.github.com>
2020-10-07 16:05:34 +0900

    itemモデルの作成②(validation,Association)

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :state, :region, :shipping_charge, :delivery_days
+
+  belongs_to :user
+
+
+  validates :price, presence: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, format:{ with: /\A[0-9]+z\/, message: 'は半角数字を使用してください' }
+  validates :name, :description, :category, :state, :region, :shipping_charge, :delivery_days, presence: true
+  validates :category_id, :state_id, :shipping_charge_id, :region_id, :delivery_days_id, numericality: { other_than: 1 }
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_074526) do
+ActiveRecord::Schema.define(version: 2020_10_06_143527) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "price", null: false
+    t.integer "category_id", null: false
+    t.integer "state_id", null: false
+    t.integer "region_id", null: false
+    t.integer "shipping_charge_id", null: false
+    t.integer "delivery_days_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
