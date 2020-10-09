18860f37fce8ec3c5a87095e8b8ee62732af5d4a
wakiyo1017 <71360557+wakiyo1017@users.noreply.github.com>
2020-10-09 14:54:33 +0900

    Itemモデル単体テストコード実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,18 +9,18 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :price, presence: true, format: { with: /\A[0-9]+\z/, message: 'は半角数字を使用してください' }, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  validates :price, presence: true, numericality: { with: /\A[0-9]+\z/, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: 'は半角数字で300円から9,999,999円の間で設定してください' }
   validates :name, presence: true
   validates :description, presence: true
-  validates :category, presence: true
+  validates :category, presence: true #item.category
   validates :state, presence: true
   validates :region, presence: true
   validates :shipping_charge, presence: true
   validates :delivery_day, presence: true
-  validates :category_id,  numericality: { other_than: 1 }
-  validates :state_id, numericality: { other_than: 1 }
-  validates :shipping_charge_id, numericality: { other_than: 1 }
-  validates :region_id, numericality: { other_than: 1 }
-  validates :delivery_day_id, numericality: { other_than: 1 }
+  validates :category_id,  numericality: { other_than: 1 , message: 'カテゴリー情報を設定してください'}
+  validates :state_id, numericality: { other_than: 1 , message: '商品の状態の情報を設定してください'}
+  validates :shipping_charge_id, numericality: { other_than: 1 , message: '配送料負担の情報を設定してください'}
+  validates :region_id, numericality: { other_than: 1 , message: '発送元の地域の情報を設定してください'}
+  validates :delivery_day_id, numericality: { other_than: 1 , message: '発送までの日数を設定してください'}
   validates :image, presence: true
 end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    association :user
+    name {Faker::Name.name}
+    description {Faker::Lorem.sentence}
+    price {10000}
+    category_id {2}
+    state_id {2}
+    shipping_charge_id {2}
+    region_id {2}
+    delivery_day_id {2}
+
+    after(:build) do |photo|
+      photo.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
