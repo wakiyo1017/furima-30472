75322bb024bdddb445db743cebe3c80853760c2f
wakiyo1017 <71360557+wakiyo1017@users.noreply.github.com>
2020-10-12 14:45:14 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.order("created_at DESC")
@@ -31,6 +31,11 @@ class ItemsController < ApplicationController
     else
       render :edit
     end      
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
   
   def set_item
