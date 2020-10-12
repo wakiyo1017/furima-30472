0915f4db78878d238bf4c432a96aaadb74c31faa
wakiyo1017 <71360557+wakiyo1017@users.noreply.github.com>
2020-10-12 12:47:10 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,19 @@ class ItemsController < ApplicationController
   def show
     @item = Item.find(params[:id])
   end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(params[:id])
+    else
+      render :edit
+    end      
+  end
   
   private
   def item_params
